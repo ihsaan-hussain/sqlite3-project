c9efd0d4ec44516615a283dc8acfca8ddfb60195
Ihsaan Hussain <ihsaan09@outlook.com>
2024-03-08 19:16:29 +0000

    created table

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 import sqlite3
+from tkinter import *
 
-print("---------------------------------------------------")
-print('\n')
+class Database:
+
+	def __init__(self):
+
+		self.conn = sqlite3.connect('Stats.db')
+		self.c = self.conn.cursor()
+
+	def create_table(self):
+		try:
+			self.c.execute("""CREATE TABLE IF NOT EXISTS players (
+				name text,
+				goals int,
+				assists int)
+				""")
+			print("Table created\n")
+		except:
+			print("There was an error")
+
+	def add_record(self):
+		self.name = input("Name of player: ")
+		self.goals = int(input("Amount of goals: "))
+		self.assists = int(input("Amount of assists: "))
+		#self.c.execute("""
+			""")
